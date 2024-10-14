ce0acf255a64444ebd815b8c59621377928cb632
UX <alonso1792@gmail.com>
2024-10-14 03:35:46 +0300

    feat: add Team section to Abous Us page

--- src/app/about_us/page.tsx
+++ src/app/about_us/page.tsx
@@ -1,6 +1,8 @@
 import { Navigation } from "@/components/nav/Navigation";
 import { AboutUsCard } from "@/components/aboutUs/AboutUsCard";
 import { AboutUsTiles } from "@/components/aboutUs/AboutUsTiles";
+
+import { Team } from "@/components/aboutUs/Team";
 import { Cta } from "@/components/footer/Cta";
 import { Footer } from "@/components/footer/Footer";
 
@@ -11,6 +13,7 @@ export default function Home() {
       <>
         <Navigation />
         <AboutUsCard />
+        <Team />
         <AboutUsTiles />
         <Cta />
         <Footer />
