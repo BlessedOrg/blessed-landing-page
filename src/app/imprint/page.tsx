4b14aa95f1fc47bbc893c5183b35279ba3cb5ab8
Filip <82238474+jumperdevweb3@users.noreply.github.com>
2024-10-15 11:16:07 +0200

    test

--- src/app/imprint/page.tsx
+++ src/app/imprint/page.tsx
@@ -2,41 +2,40 @@ import { Navigation } from "@/components/nav/Navigation";
 import { Cta } from "@/components/footer/Cta";
 import { Footer } from "@/components/footer/Footer";
 
+export default function ImprintPage() {
 
+  return (
+    <>
+      <Navigation />
+      <div className="max-w-[640px] mx-auto px-4">
+        <h2 className="font-bold uppercase text-3xl md:text-6xl mb-4">Imprint</h2>
+        <p>
+          This imprint provides important information about the owner and the legal framework of this website.
 
-export default function Home() {
+          Website Owner:
+          [Your Company Name]
+          [Your Company Address]
 
-    return (
-      <>
-        <Navigation />
-        <div className="max-w-[640px] mx-auto px-4">
-  <h2 className="font-bold uppercase text-3xl md:text-6xl mb-4">Imprint</h2>
-  <p>
-  This imprint provides important information about the owner and the legal framework of this website.
+          Business Registration:
+          [Your Registration Number]
+          [Your VAT Identification Number]
 
-Website Owner:
-[Your Company Name]
-[Your Company Address]
+          Disclaimer:
+          The content of this website is for general information purposes only. While we strive to keep the information accurate and up to date, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability, or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your
+          own risk.
 
-Business Registration:
-[Your Registration Number]
-[Your VAT Identification Number]
+          Liability for Links:
+          Through this website, you may be able to link to other websites that are not under the control of [Your Company Name]. We have no control over the nature, content, and availability of those sites. The inclusion of any links does not necessarily imply a recommendation or endorse the views expressed within them.
 
-Disclaimer:
-The content of this website is for general information purposes only. While we strive to keep the information accurate and up to date, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability, or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.
+          Copyright:
+          All content on this website, including text, graphics, logos, and images, is the property of [Your Company Name] and is protected by copyright laws. Unauthorized use or reproduction of any materials from this website is prohibited.
+        </p>
+      </div>
+      <Cta />
+      <Footer />
 
-Liability for Links:
-Through this website, you may be able to link to other websites that are not under the control of [Your Company Name]. We have no control over the nature, content, and availability of those sites. The inclusion of any links does not necessarily imply a recommendation or endorse the views expressed within them.
-
-Copyright:
-All content on this website, including text, graphics, logos, and images, is the property of [Your Company Name] and is protected by copyright laws. Unauthorized use or reproduction of any materials from this website is prohibited.
-  </p>
-</div>
-        <Cta />
-        <Footer />
-        
-      </>
-    );
-  }
+    </>
+  );
+}
 
   
--- src/components/footer/Cta.tsx
+++ src/components/footer/Cta.tsx
@@ -3,33 +3,22 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { dashboardUrl } from "@/variables/varaibles";
 
-
-
 export const Cta = () => {
   return <div className="w-full bg-[rgba(239,239,239,1)]">
-<div className="flex flex-col gap-8 justify-center py-10 items-center px-4 w-full max-w-[1280px] min-h-[212px]  mx-auto">
-
-    <div className="flex flex-col gap-6 items-center justify-center w-full max-w-[1039px] min-h-[212px] mt-10">
-    
+    <div className="flex flex-col gap-8 justify-center py-10 items-center px-4 w-full max-w-[1280px] min-h-[212px]  mx-auto">
+      <div className="flex flex-col gap-6 items-center justify-center w-full max-w-[1039px] min-h-[212px] mt-10">
         <div className="flex flex-col gap-6 md:flex-row justify-between items-center">
           <div className="flex flex-col gap-4">
-            
-          <h2 className="font-bold uppercase text-[96px] leading-[88px] md:text-6xl text-left">
-  How you manage your <span className="text-[#FFA500]">Tickets</span> matters
+            <h2 className="font-bold uppercase text-[96px] leading-[88px] md:text-6xl text-left">
+              How you manage your <span className="text-[#FFA500]">Tickets</span> matters
             </h2>
-
-          
             <p className="text-left">Join now and start building amazing experiences. Or explore <Link href={"/"} className="underline">our docs</Link>.</p>
           </div>
-         
-           <Button variant="green" className="rounded-full" size="lg" asChild>
+          <Button variant="green" className="rounded-full" size="lg" asChild>
             <Link href={`${dashboardUrl}`}>Try Blessed today</Link>
           </Button>
+        </div>
       </div>
-
-      </div>
-
-
     </div>
   </div>;
 };
