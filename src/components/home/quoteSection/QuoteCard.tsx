6e7914501bbb9ea0a39cd777318b58e8e3fc236a
UX <alonso1792@gmail.com>
2024-10-16 23:07:40 +0300

    feat(pricing): update PricingHeader section

--- src/components/home/quoteSection/QuoteCard.tsx
+++ src/components/home/quoteSection/QuoteCard.tsx
@@ -3,32 +3,34 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export const QuoteCard = () => {
-    return (
-      <div className="flex flex-col md:flex-row gap-[24px]">
-        
-        <div className="flex flex-col h-[279px] w-[624.5px] p-[24px] justify-center items-center gap-[8px] self-stretch 
-          border-[2px] rounded-[24px] border-[#1D1D1B]"
-        >
-          <div className="text-4xl font-bold" >Book a free meeting</div>
-          <div className="text-xl font-regular" >Schedule a chat with our team.</div>
-          <Link href="https://calendly.com/viet-blessed" target="_blank"> <Button variant="outline" size="default" className="rounded-[39px]">
+  return (
+    <div className="flex flex-col md:flex-row gap-[24px] pb-20">
+      
+      <div className="flex flex-col h-[279px] w-[624.5px] p-[24px] justify-center items-center gap-[8px] self-stretch 
+        border-[2px] rounded-[24px] border-[#1D1D1B]"
+      >
+        <div className="text-4xl font-bold">Book a free meeting</div>
+        <div className="text-xl font-regular">Schedule a chat with our team.</div>
+        <Link href="https://calendly.com/viet-blessed" target="_blank">
+          <Button variant="outline" size="default" className="rounded-[39px]">
             Book on Calendly
           </Button>
-          </Link>
+        </Link>
+      </div>
+      
+      <div className="flex flex-col h-[279px] w-[624.5px] p-[24px] justify-center items-center gap-[8px] self-stretch 
+        border-[2px] rounded-[24px] border-[#1D1D1B]"
+      >
+        <div className="text-4xl font-bold">Chat with us</div>
+        <div className="text-xl font-regular">
+          Join us on <Link href="https://t.me/blessedsupport" target="_blank" className="underline">Telegram</Link> or connect with our team via contact.
         </div>
-  
-     
-        <div className="flex flex-col h-[279px] w-[624.5px] p-[24px] justify-center items-center gap-[8px] self-stretch 
-          border-[2px] rounded-[24px] border-[#1D1D1B]"
-        >
-          <div className="text-4xl font-bold">Chat with us</div>
-          <div className="text-xl font-regular" >Join us on <Link href="https://t.me/blessedsupport" target="_blank" className="underline">Telegram</Link> or connect with our team via contact.</div>
-          <Link href="/contact_us"> <Button variant="outline" size="default" className="rounded-[39px]"> 
+        <Link href="/contact_us">
+          <Button variant="outline" size="default" className="rounded-[39px]"> 
             Contact us
           </Button>
-          </Link>
-        </div>
+        </Link>
       </div>
-    );
-  };
-  
+    </div>
+  );
+};
--- src/components/pricing/PricingHeader.tsx
+++ src/components/pricing/PricingHeader.tsx
@@ -63,8 +63,8 @@ export const PricingHeader = () => {
         {leftBars}
         {rightBars}
 
-        <div className="flex justify-center gap-8 mt-8">
-          <div className="flex flex-col items-start gap-4 p-8 w-[296px] h-[520px] border-2 border-[#06F881] bg-[#FFF] rounded-[24px] shadow-[0px_1px_10px_0px_rgba(0,0,0,0.06)] z-10">
+        <div className="flex justify-center gap-8 mt-8 items-start">
+          <div className="flex flex-col items-start gap-4 p-8 w-[296px]  border-2 border-[#06F881] bg-[#FFF] rounded-[24px] shadow-[0px_1px_10px_0px_rgba(0,0,0,0.06)] z-10">
             <p className="text-lg font-bold text-left">Free until 2026</p>
             <p className="text-[34px] leading-[1] font-bold tracking-[-0.5px] text-left">$0</p>
             <p className="text-[16px] text-left">
@@ -83,14 +83,14 @@ export const PricingHeader = () => {
               <img src="/img/icons/Check-Simple.svg" alt="Check icon" className="mr-2 w-6 h-6" />
               <p className="text-[16px] text-left">Community support</p>
             </div>
-            <Button variant="green" size="default" className="rounded-[39px]">
-              Started for free
+            <Button variant="green" size="default" className="w-[232px] h-[52px] rounded-[39px] mt-14">
+              Start
             </Button>
           </div>
 
-          <div className="flex flex-col items-start gap-4 p-8 w-[296px] h-[520px] border-2 border-black bg-[#FFF] rounded-[24px] shadow-[0px_1px_10px_0px_rgba(0,0,0,0.06)] z-10">
+          <div className="flex flex-col items-start gap-4 p-8 w-[296px] border-2 border-black bg-[#FFF] rounded-[24px] shadow-[0px_1px_10px_0px_rgba(0,0,0,0.06)] z-10">
             <p className="text-lg font-bold text-left">Commitment plan</p>
-            <p className="text-[34px] leading-[1] font-bold tracking-[-0.5px] text-left">$80/mo</p>
+            <p className="text-[34px] leading-[1] font-bold tracking-[-0.5px] text-left">$69/mo*</p>
             <p className="text-[16px] text-left">
               Get in touch for tailored pricing. Best for large-scale and recurring events.
             </p>
@@ -99,7 +99,7 @@ export const PricingHeader = () => {
               <img src="/img/icons/Check-Simple.svg" alt="Check icon" className="mr-2 w-6 h-6" />
               <p className="text-[16px] text-left">White-glove service</p>
             </div>
-            <Button variant="outline" size="default" className="rounded-[39px]">
+            <Button variant="outline" size="default" className="w-[232px] h-[52px] rounded-[39px] mt-14">
               Contact us
             </Button>
           </div>
