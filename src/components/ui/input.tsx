43aa81b353e2221fe9d08732bd4b13ebac255e15
Alexander Voynovskiy <alonso1792@gmail.com>
2024-10-29 16:40:55 +0200

    style(AuthModal): update focus border thickness

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -10,7 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus:outline-none focus:ring-0 focus:border-[#6157FF]",
+          "flex h-10 w-full rounded-md border-2 border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus:outline-none focus:ring-0 focus:border-[#6157FF]",
           className
         )}
         ref={ref}
